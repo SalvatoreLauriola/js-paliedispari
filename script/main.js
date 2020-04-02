b2936f12ff5b52dfc245e1d3524809355fdf2794
Salvatore Lauriola <salvatore6324@gmail.com>
2020-04-02 15:29:13 +0200

    problem

--- script/main.js
+++ script/main.js
@@ -1,31 +1,85 @@
+// Esercizio 1
+
 // 1 Chiedere all’utente di inserire una parola;
 
-var parola = prompt('Inserisci una parola');
-// console.log(parola)
+// var parola = prompt('Inserisci una parola');
+// // console.log(parola)
 
-// 2 Creare una funzione per capire se la parola inserita è palindroma
+// // 2 Creare una funzione per capire se la parola inserita è palindroma
 
-var parolaReverse = reverseChar(parola);
+// // invocazione
+// var parolaReverse = reverseChar(parola);
 
-console.log('Parola');
-console.log(parola);
-console.log(parolaReverse);
+// console.log('Parola');
+// console.log(parola);
+// console.log(parolaReverse);
 
-if (parola == parolaReverse) {
-  console.log('La parola inserita è palindroma');
+// if (parola == parolaReverse) {
+//   console.log('La parola inserita è palindroma');
 
-}else {
-  console.log('La parola inserita non è palindroma');
+// }else {
+//   console.log('La parola inserita non è palindroma');
+// }
+
+// function reverseChar (word) {
+
+//   var reverse = '';
+
+//   for (var i = word.length -1; i >= 0; i--) {
+//     reverse += word[i];
+//   }
+  
+//   return reverse;
+// }
+
+
+// Esercizio 2 
+
+// L’utente sceglie pari o dispari e un numero da 1 a 5. 
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+var pariDispari = prompt('Inserisci pari o dispari');
+
+var numeroUtente = parseInt(prompt('Inserisci numero tra 1 e 5'))
+
+var rand;
+
+var pcNumber = getRandomNumber(rand);
+
+console.log(numeroUtente);
+console.log(pcNumber);
+
+var somma = numeroUtente + pcNumber;
+
+console.log(somma);
+
+function getRandomNumber (number) {
+
+  var rand = Math.floor(Math.random() *5) + 1 ;
+
+  return rand;
+
 }
+ 
 
+function parDisp(numero) {
+    if (numero % 2 == 0){
+      return 'pari';
+    }
 
-function reverseChar (word) {
-
-  var reverse = '';
-
-  for (var i = word.length -1; i >= 0; i--) {
-    reverse += word[i];
+    return 'dispari';
   }
 
-  return reverse;
-}
+  if (parDisp(somma) == 'pari'){
+  console.log('Hai vinto')
+
+
+  }else {
+    console.log('Hai perso');
+  }
+
+  
